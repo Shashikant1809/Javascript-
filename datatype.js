e2c3c944a1d07d5b1fcbdf34d2f0f184ae942b3d
Shashikant1809 <shashikantverma1809@gmail.com>
2024-04-02 17:33:23 +0530

    datatypes confusion

--- datatype.js
+++ datatype.js
@@ -3,3 +3,22 @@
 // alert("hellow");// we are using node js not browser
 console.log(3 + 3); //code readability should be high
 
+// types of datatypes
+let name = "shashikant";
+let age = 12;
+let isMale = true;
+// range of number = 2 to power 53
+//Big_int
+//String
+//boolean=>true/false
+//null  => stand alone value
+//undefined => value is not assigned yet
+//symbol=>unique ke liye use hota hai
+
+//object
+console.log(typeof "shash");
+
+// typeof operater tells about the datatypes
+// type of null is Object but type of undefined is undefined
+// undefined is a type itself
+
